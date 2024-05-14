4bb316f7a320ff753a5fb4da549ea1c9bb9967b9
Apurv Sonawane <apurvsonawane28@gmail.com>
2024-05-14 17:02:24 +0530

    Fixed the user page

--- vjti_canteen_admin/src/components/admin/users/User.js
+++ vjti_canteen_admin/src/components/admin/users/User.js
@@ -2,7 +2,7 @@ const User = ({ user }) => {
   const capitalizedUserName = user.name.charAt(0).toUpperCase() + user.name.slice(1);
 
   return (
-    <div className="w-full md:w-1/2 lg:w-1/2 p-2">
+    <div className="w-[250px] mr-5 p-2">
       <div className="bg-white rounded-lg shadow-md p-5" style={{ minWidth: '250px',margin:'10px' }}>
         <div className="flex flex-col items-start space-y-3">
           <div className="text-sm font-medium text-slate-900">{capitalizedUserName}</div>
